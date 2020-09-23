3362abf9b3bb3fe8e7fd3f20476d9fc7a57b7cab
bethel bashorun <bashorunbethel99@gmail.com>
2020-09-23 19:12:09 +0100

    third commit

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -3,6 +3,13 @@ import * as firebase from "firebase/app";
 import 'firebase/firestore'
 import 'firebase/database'
 
+const twilio = require('twilio');
+const accountSid = firebase.firestore.config().twilio.sid
+const authToken = firebase.firestore.config().twilio.token
+const client = new twilio(accountSid, authToken);
+const twilioNumber = '(279) 205-4373'
+
+
 export class AppConfig {
     constructor() { }//singleton
     displayMessage(msg: string, success: boolean) {
@@ -41,4 +48,30 @@ export class AppConfig {
             }
         }
     }
+    validE164(num) {
+        return /^\*?[1-9]\d{1, 14}$/.test(num)
+    }
+    exports.textStatus = firebase.database.ref('/orders/{orderKey}/status').onUpdate(event => {
+        const orderKey = event.params.orderKey
+
+        return admin.database().ref(`/orders/${orderKey}`).once("value").then(snapshot => snapshot.val()).then(order => {
+            const stauts = order.stauts
+            const phoneNumber = order.phoneNumber
+
+
+            if (!validE164(phoneNumber)) {
+                throw new Error('number must be E164 format!')
+            }
+
+            const textMessage = {
+                body: `Current order status: ${status}`,
+                to: phoneNumber,
+                from: twilioNumber
+            }
+
+            return client.messages.create(textMessage)
+        }).then(Message => console.log(message.sid, 'succes'))
+            .catch(err => console.log(err))
+    });
+
 }
